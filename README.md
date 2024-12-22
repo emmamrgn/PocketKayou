f815f552115ebdb152b88bcaa004416ce049b4f8
emmamrgn <dev.emmamrgn@gmail.com>
2024-12-22 18:58:11 +0100

    rm

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sub-role-bot
+
+## Description :
+
+#### bot qui met à jour sur mon serveur les rôles ``subs twitch`` en fonctions de si on est sub à 2 personnes ou pas
+
--- logs.py
+++
@@ -1,7 +0,0 @@
-import discord
-from global_var import CHANNEL_ID
-
-async def log_role_change(member, role, action):
-    channel = member.guild.get_channel(CHANNEL_ID)
-    if channel:
-        await channel.send(f"🔄 {action} rôle '{role.name}' pour {member.display_name}")
